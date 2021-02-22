ac3c6818eaad7760b56779c20852f12b18530a54
Shruthi Lakshmanan Parthasarathy <lpshruthi@gmail.com>
2021-02-22 16:48:31 +0100

    done changes

---
+++ project_history/lib.py
@@ -0,0 +1,24 @@
+def try_me(num):
+
+    """
+    calculates curiosity based on input  """
+
+    if num<100:
+        print("you seem to be interested in galaxies")
+    else:
+        print("you seem to be interested in mankind")
+
+
+
+
+if __name__ == "__main__":
+
+    # Le Wagon location
+    num=53
+
+    curiosity = try_me(num)
+
+
+
+
+
